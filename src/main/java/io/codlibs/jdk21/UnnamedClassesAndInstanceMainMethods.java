c123eab46a47277d2fa0dfe3cf38906656301f2f
Manoj Sharma <manoj.mahijpr@gmail.com>
2023-11-21 17:34:37 +0530

    JEP 445: Unnamed Classes and Instance Main Methods (Preview)

---
+++ src/main/java/io/codlibs/jdk21/UnnamedClassesAndInstanceMainMethods.java
@@ -0,0 +1,57 @@
+package io.codlibs.jdk21;
+
+/*
+    // main1
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // main2
+    static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // main3
+    void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // main4
+    void main() {
+        System.out.println("Hello World");
+    }
+*/
+
+public class UnnamedClassesAndInstanceMainMethods {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /*
+    // main1
+    public static void main() {
+        System.out.println("Hello World");
+    }*/
+
+    /*
+    // main2
+    static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    */
+
+    /*
+    // main3
+    void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    */
+
+    /*
+    // main4
+    void main() {
+        System.out.println("Hello World");
+    }
+    */
+}
